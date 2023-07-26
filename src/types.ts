ef2fc91453b5d9138bbc1e9297a283e6bca0fd1e
muje hoxe <mujehoxe@gmail.com>
2023-07-26 13:12:19 +0100

    a section has title and dates

--- src/types.ts
+++ src/types.ts
@@ -20,6 +20,9 @@ type SectionContentTypes = 'video' | 'markdown' | 'pdf' | 'image' | 'svg';
 
 interface Section {
   id: number;
+  title: string;
+  dateCreated: Date;
+  dateModified: Date;
   type: 'lecture' | 'exercice' | 'solution';
   contentType: SectionContentTypes;
   url: string;
