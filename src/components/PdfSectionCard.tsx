bf2025244fb6e297f695ffe2fe2469d54c2cc873
muje hoxe <mujehoxe@gmail.com>
2023-07-28 23:58:09 +0100

    Pdf Card style

--- src/components/PdfSectionCard.tsx
+++ src/components/PdfSectionCard.tsx
@@ -5,7 +5,7 @@ import {TouchableOpacity, View} from 'react-native';
 import {Text} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {PdfViewScreenNavigationProp} from '../screens/PdfView';
-import {ArrowRightIcon, DocumentIcon} from 'react-native-heroicons/outline';
+import {ArrowRightIcon, DocumentIcon} from 'react-native-heroicons/solid';
 
 const PdfSectionCard: React.FC<SectionCardProps> = ({section}) => {
   const navigation = useNavigation<PdfViewScreenNavigationProp>();
@@ -15,12 +15,12 @@ const PdfSectionCard: React.FC<SectionCardProps> = ({section}) => {
       onPress={() => {
         navigation.navigate('PdfView', {section});
       }}
-      style={tw`bg-white shadow-md rounded-md p-4 flex-row items-center justify-between`}>
-      <View style={tw`flex-row items-center`}>
-        <DocumentIcon size={24} color="gray" style={tw`mr-2`} />
+      style={tw`bg-white shadow-md rounded-md p-4 flex-col items-center justify-between`}>
+      <View style={tw`flex-row items-center h-36`}>
+        <DocumentIcon size={30} style={tw`mr-2 text-red-400`} />
         <Text style={tw`text-slate-900 text-base`}>Jump To PDF</Text>
       </View>
-      <ArrowRightIcon size={24} color="gray" />
+      <ArrowRightIcon size={30} color="teal" style={tw`text-teal-700`} />
     </TouchableOpacity>
   );
 };
