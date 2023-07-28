585a965abd5be7098f3e504588d603c43fa3a030
muje hoxe <mujehoxe@gmail.com>
2023-07-28 21:53:27 +0100

    svg section card; named exports

--- src/components/SectionContentTypeRenderers.tsx
+++ src/components/SectionContentTypeRenderers.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import {SectionCardProps, SectionContentTypes} from '../types';
-import tw from 'twrnc';
-import {View, Image} from 'react-native';
-import {SvgUri} from 'react-native-svg';
-import {MarkdownSectionCard} from './MarkdownSectionCard';
-import {VideoSectionCard} from './VideoSectionCard';
-import {PdfSectionCard} from './PdfSectionCard';
-import {ImageSectionCard} from './ImageSectionCard';
-
-export const SvgSectionCard: React.FC<SectionCardProps> = ({section}) => (
-  <View style={tw`w-full h-72`}>
-    <SvgUri uri={section.src} width="100%" height="100%" fill="#000000" />
-  </View>
-);
+import MarkdownSectionCard from './MarkdownSectionCard';
+import VideoSectionCard from './VideoSectionCard';
+import PdfSectionCard from './PdfSectionCard';
+import ImageSectionCard from './ImageSectionCard';
+import SvgSectionCard from './SvgSectionCard';
 
 export const contentRenderers: {
   [key in SectionContentTypes]: React.FC<SectionCardProps>;
