b0942944192bcc065d8d545932cfdcd3a0f4b70e
muje hoxe <mujehoxe@gmail.com>
2023-07-31 20:21:04 +0100

    SectionCard rtl with arabic date format

--- src/components/SectionCard.tsx
+++ src/components/SectionCard.tsx
@@ -4,12 +4,21 @@ import {contentRenderers} from './SectionContentTypeRenderers';
 import ErrorComponent from './ErrorComponent';
 import {Text, View} from 'react-native';
 import tw from 'twrnc';
+import {useTranslation} from 'react-i18next';
 
 const SectionCard: React.FC<SectionCardProps> = ({
   section,
   isPlaying,
   onPress,
 }) => {
+  const {t, i18n} = useTranslation();
+  const formatter = new Intl.DateTimeFormat(i18n.languages[0], {
+    dateStyle: 'full',
+    weekday: 'long',
+    day: '2-digit',
+    month: 'long',
+  });
+
   const ContentComponent = contentRenderers[section.contentType];
 
   if (!ContentComponent) {
@@ -33,7 +42,7 @@ const SectionCard: React.FC<SectionCardProps> = ({
       {assignmentSection.deadline && (
         <View style={tw`p-4 bg-red-100 border-t border-gray-300 rounded-b-md`}>
           <Text style={tw`text-right text-red-600 text-base font-semibold`}>
-            Deadline: {assignmentSection.deadline.toDateString()}
+            {t('deadline') + formatter.format(assignmentSection.deadline)}
           </Text>
         </View>
       )}
