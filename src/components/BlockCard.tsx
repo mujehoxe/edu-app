297712704289d7a2201b8029277ed28825afda81
muje hoxe <mujehoxe@gmail.com>
2023-08-20 12:29:27 +0100

    margin instead of vertical centering of empty list

--- src/components/BlockCard.tsx
+++ src/components/BlockCard.tsx
@@ -49,9 +49,9 @@ const BlockCard: React.FC<BlockCardProps> = ({block}) => {
         data={topics}
         renderItem={renderTopic}
         keyExtractor={item => item._id.toString()}
-        contentContainerStyle={tw`my-auto pb-8`}
+        contentContainerStyle={tw`my-autopb-8`}
         ListEmptyComponent={
-          <View style={tw`flex-1 justify-center items-center`}>
+          <View style={tw`my-6 flex-1 justify-center items-center`}>
             <Text>{t('noTopics')}</Text>
           </View>
         }
--- src/screens/UnitDetails.tsx
+++ src/screens/UnitDetails.tsx
@@ -47,9 +47,9 @@ const UnitDetails: React.FC<UnitDetailsProps> = ({route}) => {
         data={blocks}
         renderItem={renderBlock}
         keyExtractor={item => item._id.toString()}
-        contentContainerStyle={tw`my-auto pb-8`}
+        contentContainerStyle={tw`pb-8`}
         ListEmptyComponent={
-          <View style={tw`flex-1 justify-center items-center`}>
+          <View style={tw`my-6 flex-1 justify-center items-center`}>
             <Text style={tw`text-slate-700 text-lg`}>{t('noBlocks')}</Text>
           </View>
         }
