83c0878b90acafb5553f1fc4709fbe2f531973f5
muje hoxe <mujehoxe@gmail.com>
2023-08-26 23:48:03 +0100

    Dark login

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -3,24 +3,34 @@ import {StatusBar, Text, View} from 'react-native';
 import tw from '../../tailwind';
 import useGoogleOneTapSignIn from '../hooks/useGoogleOneTapSignIn';
 import SignInWithGoogle from '../components/SignInWithGoogleButton';
+import {useAppColorScheme} from 'twrnc';
+import SystemNavigationBar from 'react-native-system-navigation-bar';
 
 export function LogIn(): ReactElement {
   const [isGoogleSignInButtonVisible, setIsGoogleSignInButtonVisible] =
     useState<boolean>(false);
 
+  const [colorScheme] = useAppColorScheme(tw);
+
   useEffect(() => {
-    StatusBar.setBackgroundColor('white');
-    StatusBar.setBarStyle('dark-content');
-  }, []);
+    if (colorScheme === 'light') {
+      SystemNavigationBar.setNavigationColor('white');
+      StatusBar.setBarStyle('dark-content');
+    } else {
+      SystemNavigationBar.setNavigationColor('black');
+      StatusBar.setBarStyle('light-content');
+    }
+  }, [colorScheme]);
 
   useGoogleOneTapSignIn(setIsGoogleSignInButtonVisible);
 
   return (
-    <View style={tw`flex-1 justify-center items-center bg-white`}>
-      <Text style={tw`text-3xl font-bold text-blue-500 mb-2`}>
-        Welcome to Your Appp
+    <View style={tw`flex-1 justify-center items-center bg-white dark:bg-black`}>
+      <Text style={tw`text-3xl font-bold text-primary mb-2`}>
+        Welcome to Your App
       </Text>
-      <Text style={tw`text-center text-lg text-gray-600 mb-6`}>
+      <Text
+        style={tw`text-center text-lg text-gray-600 dark:text-gray-400 mb-6`}>
         Sign in to access your personalized experience.
       </Text>
       <View style={isGoogleSignInButtonVisible ? tw`` : tw`opacity-0`}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2196F3',
+      },
+    },
   },
   plugins: [],
 };
