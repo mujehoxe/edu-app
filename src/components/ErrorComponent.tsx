7d88fd8dec402fee2650335d1400fc46374535c3
muje hoxe <mujehoxe@gmail.com>
2023-08-28 17:59:08 +0100

    error component refactor style

--- src/components/ErrorComponent.tsx
+++ src/components/ErrorComponent.tsx
@@ -8,8 +8,8 @@ interface ErrorScreenProps {
 
 const ErrorComponent: React.FC<ErrorScreenProps> = ({message}) => {
   return (
-    <View style={tw`flex-1 bg-white justify-center items-center`}>
-      <View style={tw`bg-red-100 px-4 py-2 rounded-lg mb-4`}>
+    <View>
+      <View style={tw`bg-red-100 px-4 py-4 rounded-lg my-4`}>
         <Text style={tw`text-red-600 text-xl font-semibold`}>Error:</Text>
         <Text style={tw`text-red-600 text-base`}>{message}</Text>
       </View>
