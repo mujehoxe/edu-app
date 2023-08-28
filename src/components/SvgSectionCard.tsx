b0d32a02a3d29d076a77623ac52a9b896d4ca6b8
muje hoxe <mujehoxe@gmail.com>
2023-08-28 18:59:14 +0100

    dark svg

--- src/components/SvgSectionCard.tsx
+++ src/components/SvgSectionCard.tsx
@@ -5,8 +5,7 @@ import {View} from 'react-native';
 import {SvgUri} from 'react-native-svg';
 
 const SvgSectionCard: React.FC<SectionCardProps> = ({section}) => (
-  <View
-    style={tw`bg-white border border-gray-300 shadow-md rounded-md overflow-hidden w-full h-72`}>
+  <View style={tw`w-full h-72`}>
     <SvgUri uri={section.src} width="100%" height="100%" fill="#000000" />
   </View>
 );
