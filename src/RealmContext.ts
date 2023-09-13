5c6641072c4af03182c4992a0c3b03e7cec5ab12
muje hoxe <mujehoxe@gmail.com>
2023-09-13 15:56:37 +0100

    realm removed

--- src/RealmContext.ts
+++
@@ -1,7 +0,0 @@
-import {createRealmContext} from '@realm/react';
-import {Block, Section, Topic, Unit} from './Schemas';
-
-export const realmContext = createRealmContext({
-  schema: [Unit, Block, Topic, Section],
-  schemaVersion: 7,
-});
