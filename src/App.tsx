2cf7232372a51638d0bc9958ddb664cdd707338c
muje hoxe <mujehoxe@gmail.com>
2023-09-13 16:17:19 +0100

    refactor user types

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import {MainApp} from './MainApp';
 import tw from '../tailwind';
 import {useDeviceContext} from 'twrnc';
 import {Login} from './screens/Login';
-import auth from '@react-native-firebase/auth';
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+import {LoadingIndicator} from './components/LoadingIndicator';
 
 const App: React.FC = () => {
   useHideSplashScreen();
@@ -12,22 +13,22 @@ const App: React.FC = () => {
   useDeviceContext(tw);
 
   const [initializing, setInitializing] = useState<boolean>(true);
-  const [user, setUser] = useState();
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
 
   useEffect(() => {
-    function onAuthStateChanged(user) {
-      setUser(user);
+    function onAuthStateChanged(newUser: FirebaseAuthTypes.User | null) {
+      setUser(newUser);
       if (initializing) {
         setInitializing(false);
       }
     }
 
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
+    return () => subscriber(); // Unsubscribe on unmount
   }, [initializing]);
 
   if (initializing) {
-    return null;
+    return <LoadingIndicator />;
   }
 
   if (!user) {
